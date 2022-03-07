9ad182c8f4d06b8da67995d276809f0a0868330a
Javilingys <shkola.govno@gmail.com>
2022-03-07 20:23:49 +0700

    New Project Basket Service

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        public BasketController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetBasket()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult UpdateBasket()
+        {
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBasket()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateCheckout()
+        {
+
+        }
+    }
+}
