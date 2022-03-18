e102b1887c3b7f0a80509225b9ecd251c78bbee3
Javilingys <shkola.govno@gmail.com>
2022-03-18 16:28:52 +0700

    GetOrder Qurry and CheckoutOrder Command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderLit = await _orderRepository.GetOrdersByUserName(request.UserName);
+            var result = _mapper.Map<List<OrdersVm>>(orderLit);
+            return result;
+        }
+    }
+}
